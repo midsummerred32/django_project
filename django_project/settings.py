ed58de9577c20de7bb8227694270a113441c2fce
midsummerred32 <trevorleavitt08@gmail.com>
2021-07-23 08:17:43 -0400

    jo

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config.django_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['mids-website.herokuapp.com']
 
