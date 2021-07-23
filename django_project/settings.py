1be72dcd1a72e514ee88acb346b88b02d5a3ade8
midsummerred32 <trevorleavitt08@gmail.com>
2021-07-23 08:32:17 -0400

    d

--- django_project/settings.py
+++ django_project/settings.py
@@ -149,6 +149,6 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
